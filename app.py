0ce671e31e13b1dd3c0a8466732ce615fcd626cf
Branden Jordan <lordkyzr@gmail.com>
2021-07-14 16:56:53 -0700

    Added exceptions class and modified how the app verifies the session

--- app.py
+++ app.py
@@ -1,20 +1,22 @@
 import os
 import uuid
 
-from flask import Flask, jsonify, session, request, redirect
+from flask import Flask, jsonify, session, request, redirect, render_template
 from flask_session import Session
 import spotipy
-from spotipy.oauth2 import SpotifyOAuth
 
 from app_secrets import CLIENT_ID, CLIENT_SECRET, REDIRECT_URI
+from exceptions import SessionInvalid
 
-
-app = Flask(__name__)
+app = Flask(__name__,
+            static_url_path='',
+            static_folder='static',
+            template_folder='static/templates')
 app.config['SESSION_TYPE'] = 'filesystem'
 app.config['SESSION_FILE_DIR'] = './.flask_session/'
 Session(app)
 
-
+SCOPE = "user-library-read user-read-currently-playing user-top-read"
 caches_folder = './.spotify_caches/'
 if not os.path.exists(caches_folder):
     os.makedirs(caches_folder)
@@ -22,6 +24,22 @@ if not os.path.exists(caches_folder):
 
 def session_cache_path():
     return caches_folder + session.get('uuid')
+
+
+def verify_session(scope):
+    cache_handler = spotipy.cache_handler.CacheFileHandler(cache_path=session_cache_path())
+    auth_manager = spotipy.oauth2.SpotifyOAuth(
+        client_id=CLIENT_ID,
+        client_secret=CLIENT_SECRET,
+        redirect_uri=REDIRECT_URI,
+        scope=scope,
+        cache_handler=cache_handler
+    )
+
+    if auth_manager.validate_token(cache_handler.get_cached_token()):
+        return cache_handler, auth_manager
+    else:
+        raise SessionInvalid()
 
 
 @app.route('/')
@@ -35,7 +53,7 @@ def index():
         client_id=CLIENT_ID,
         client_secret=CLIENT_SECRET,
         redirect_uri=REDIRECT_URI,
-        scope='user-library-read user-read-currently-playing',
+        scope=SCOPE,
         cache_handler=cache_handler
     )
 
@@ -47,18 +65,24 @@ def index():
     if not auth_manager.validate_token(cache_handler.get_cached_token()):
         # Step 2. Display sign in link when no token
         auth_url = auth_manager.get_authorize_url()
-        return f'<marquee><h1>Welcome to Branden\'s Spotify Hackathon Project</h1></marquee>' \
-               f'<h2> Click Sign In to link your Spotify account' \
-               f'<br/><a href="{auth_url}">Sign in</a></h2>'
+        return render_template("index.html", auth_url=auth_url)
 
     # Step 4. Signed in, display data
-    sp = spotipy.Spotify(auth_manager=auth_manager)
-    results = sp.current_user_saved_tracks()
-    artists = set()
-    for item in results["items"]:
-        for artist in item["track"]["artists"]:
-            artists.add(artist["name"])
-    return jsonify(list(artists))
+    sp_api_client = spotipy.Spotify(auth_manager=auth_manager)
+    results = sp_api_client.current_user_saved_tracks(limit=50)
+
+    songs = list()
+
+    for song in results["items"]:
+        song_dict = dict()
+        song_dict["name"] = song["track"]["name"]
+        song_dict["album_art"] = song["track"]["album"]["images"][1]["url"]
+        song_dict["artists"] = [artist["name"] for artist in song["track"]["artists"]]
+        song_dict["added_time"] = song["added_at"]
+        song_dict["preview_url"] = song["track"]["preview_url"]
+        songs.append(song_dict)
+
+    return render_template("index.html", songs=songs, uuid=session['uuid'])
 
 
 @app.route('/sign_out')
@@ -75,7 +99,14 @@ def sign_out():
 @app.route('/currently_playing')
 def currently_playing():
     cache_handler = spotipy.cache_handler.CacheFileHandler(cache_path=session_cache_path())
-    auth_manager = spotipy.oauth2.SpotifyOAuth(cache_handler=cache_handler)
+    auth_manager = spotipy.oauth2.SpotifyOAuth(
+        client_id=CLIENT_ID,
+        client_secret=CLIENT_SECRET,
+        redirect_uri=REDIRECT_URI,
+        scope=SCOPE,
+        cache_handler=cache_handler
+    )
+
     if not auth_manager.validate_token(cache_handler.get_cached_token()):
         return redirect('/')
     spotify = spotipy.Spotify(auth_manager=auth_manager)
@@ -84,18 +115,13 @@ def currently_playing():
         return track
     return "No track currently playing."
 
-#
-# @app.route("/saved_tracks")
-# def spotify_auth():
-#     sp = spotipy.Spotify(auth_manager=SpotifyOAuth(client_id=CLIENT_ID,
-#                                                    client_secret=CLIENT_SECRET,
-#                                                    redirect_uri=REDIRECT_URI,
-#                                                    scope="user-library-read"))
-#
-#     results = sp.current_user_saved_tracks()
-#     artists = set()
-#     for item in results["items"]:
-#         for artist in item["track"]["artists"]:
-#             artists.add(artist["name"])
-#     return jsonify(list(artists))
-#     # return jsonify(results["items"])
+
+@app.route('/top_artists')
+def top_artists():
+    try:
+        cache_handler, auth_manager = verify_session(SCOPE)
+    except SessionInvalid:
+        return redirect('/')
+
+    sp_api_client = spotipy.Spotify(auth_manager=auth_manager)
+    return jsonify(sp_api_client.current_user_top_artists(limit=9))
---
+++ exceptions.py
@@ -0,0 +1,2 @@
+class SessionInvalid(Exception):
+    pass
